02ed9344febce24daef1a6dad7ebd1983b933c9f
Oliver Klee <github@oliverklee.de>
2018-04-21 18:45:10 +0200

    [CLEANUP] Always use ::class (#41)

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,4 @@ defined('TYPO3_MODE') or die('Access denied.');
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['tx_oelib_Double3Validator'] = 'EXT:oelib/Classes/Double3Validator.php';
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['oelib']['testingFrameworkCleanUp'][]
-    = 'EXT:oelib/Classes/TestingFrameworkCleanup.php:Tx_Oelib_TestingFrameworkCleanup';
+    = 'EXT:oelib/Classes/TestingFrameworkCleanup.php:' . \Tx_Oelib_TestingFrameworkCleanup::class;
