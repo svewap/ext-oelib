b4d0edd486c1ecf8d6e9bea223c5d864a632fe06
Oliver Klee <github@oliverklee.de>
2018-11-06 13:17:10 +0100

    [CLEANUP] Streamline the ext_emconf.php of the test extensions (#135)

--- TestExtensions/user_oelibtest/ext_emconf.php
+++ TestExtensions/user_oelibtest/ext_emconf.php
@@ -3,31 +3,14 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Test extension for tx_oelib',
     'description' => 'Test extension for tx_oelib',
-    'category' => 'example',
-    'author' => 'Niels Pardon',
-    'author_email' => 'mail@niels-pardon.de',
-    'shy' => '',
-    'dependencies' => 'oelib',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'alpha',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'author_company' => '',
     'version' => '2.1.0',
+    'category' => 'example',
     'constraints' => [
         'depends' => [
             'oelib' => '',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
     ],
-    '_md5_values_when_last_written' => '',
+    'state' => 'experimental',
+    'author' => 'Niels Pardon',
+    'author_email' => 'mail@niels-pardon.de',
 ];
--- TestExtensions/user_oelibtest2/ext_emconf.php
+++ TestExtensions/user_oelibtest2/ext_emconf.php
@@ -3,32 +3,15 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Second test extension for tx_oelib',
     'description' => 'Second test extension for tx_oelib',
-    'category' => 'example',
-    'author' => 'Niels Pardon',
-    'author_email' => 'mail@niels-pardon.de',
-    'shy' => '',
-    'dependencies' => 'oelib,user_oelibtest',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
-    'state' => 'alpha',
-    'internal' => '',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearCacheOnLoad' => 0,
-    'lockType' => '',
-    'author_company' => '',
     'version' => '2.1.0',
+    'category' => 'example',
     'constraints' => [
         'depends' => [
             'oelib' => '',
             'user_oelibtest' => '',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
     ],
-    '_md5_values_when_last_written' => '',
+    'state' => 'experimental',
+    'author' => 'Niels Pardon',
+    'author_email' => 'mail@niels-pardon.de',
 ];
