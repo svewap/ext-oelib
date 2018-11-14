c7b698fb24877fe256e8cdedf6aa69a547be6d84
Oliver Klee <github@oliverklee.de>
2018-11-14 15:22:45 +0100

    [CLEANUP] Use spaces instead of tabs for SQL files (#153)

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,35 +2,35 @@
 # Table structure for table 'tx_oelib_test'
 #
 CREATE TABLE tx_oelib_test (
-	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-	pid int(11) unsigned DEFAULT '0' NOT NULL,
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	object_type int(11) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-	title varchar(255) DEFAULT '' NOT NULL,
-	friend int(11) unsigned DEFAULT '0' NOT NULL,
-	owner int(11) unsigned DEFAULT '0' NOT NULL,
-	children varchar(255) DEFAULT '' NOT NULL,
-	related_records int(11) unsigned DEFAULT '0' NOT NULL,
-	bidirectional int(11) unsigned DEFAULT '0' NOT NULL,
-	composition int(11) unsigned DEFAULT '0' NOT NULL,
-	composition2 int(11) unsigned DEFAULT '0' NOT NULL,
-	float_data float(9,6) DEFAULT '0.000000' NOT NULL,
-	decimal_data decimal(10,3) DEFAULT '0.000' NOT NULL,
-	string_data varchar(255) DEFAULT '' NOT NULL,
-	header varchar(255) DEFAULT '' NOT NULL,
+    uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    object_type int(11) unsigned DEFAULT '0' NOT NULL,
+    hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    starttime int(11) unsigned DEFAULT '0' NOT NULL,
+    endtime int(11) unsigned DEFAULT '0' NOT NULL,
+    title varchar(255) DEFAULT '' NOT NULL,
+    friend int(11) unsigned DEFAULT '0' NOT NULL,
+    owner int(11) unsigned DEFAULT '0' NOT NULL,
+    children varchar(255) DEFAULT '' NOT NULL,
+    related_records int(11) unsigned DEFAULT '0' NOT NULL,
+    bidirectional int(11) unsigned DEFAULT '0' NOT NULL,
+    composition int(11) unsigned DEFAULT '0' NOT NULL,
+    composition2 int(11) unsigned DEFAULT '0' NOT NULL,
+    float_data float(9,6) DEFAULT '0.000000' NOT NULL,
+    decimal_data decimal(10,3) DEFAULT '0.000' NOT NULL,
+    string_data varchar(255) DEFAULT '' NOT NULL,
+    header varchar(255) DEFAULT '' NOT NULL,
 
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY dummy (is_dummy_record),
-	KEY object_type (object_type)
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY dummy (is_dummy_record),
+    KEY object_type (object_type)
 );
 
 
@@ -38,21 +38,21 @@ CREATE TABLE tx_oelib_test (
 # Table structure for table 'tx_oelib_testchild'
 #
 CREATE TABLE tx_oelib_testchild (
-	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
-	pid int(11) unsigned DEFAULT '0' NOT NULL,
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	title varchar(255) DEFAULT '' NOT NULL,
-	parent int(11) unsigned DEFAULT '0' NOT NULL,
-	tx_oelib_parent2 int(11) unsigned DEFAULT '0' NOT NULL,
+    uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+    pid int(11) unsigned DEFAULT '0' NOT NULL,
+    tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+    crdate int(11) unsigned DEFAULT '0' NOT NULL,
+    cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting int(11) unsigned DEFAULT '0' NOT NULL,
+    deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    title varchar(255) DEFAULT '' NOT NULL,
+    parent int(11) unsigned DEFAULT '0' NOT NULL,
+    tx_oelib_parent2 int(11) unsigned DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY dummy (is_dummy_record)
+    PRIMARY KEY (uid),
+    KEY parent (pid),
+    KEY dummy (is_dummy_record)
 );
 
 
@@ -60,14 +60,14 @@ CREATE TABLE tx_oelib_testchild (
 # Table structure for table 'tx_oelib_test_article_mm'
 #
 CREATE TABLE tx_oelib_test_article_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+    uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting int(11) unsigned DEFAULT '0' NOT NULL,
+    is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
 
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign),
-	KEY dummy (is_dummy_record)
+    KEY uid_local (uid_local),
+    KEY uid_foreign (uid_foreign),
+    KEY dummy (is_dummy_record)
 );
 
 
@@ -75,8 +75,8 @@ CREATE TABLE tx_oelib_test_article_mm (
 # Table structure for table 'be_users'
 #
 CREATE TABLE be_users (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -84,8 +84,8 @@ CREATE TABLE be_users (
 # Table structure for table 'be_groups'
 #
 CREATE TABLE be_groups (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -93,8 +93,8 @@ CREATE TABLE be_groups (
 # Table structure for table 'fe_groups'
 #
 CREATE TABLE fe_groups (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -102,8 +102,8 @@ CREATE TABLE fe_groups (
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -111,8 +111,8 @@ CREATE TABLE fe_users (
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -120,8 +120,8 @@ CREATE TABLE pages (
 # Table structure for table 'sys_template'
 #
 CREATE TABLE sys_template (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -129,16 +129,16 @@ CREATE TABLE sys_template (
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 #
 # Table structure for table 'sys_file'
 #
 CREATE TABLE sys_file (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -146,8 +146,8 @@ CREATE TABLE sys_file (
 # Table structure for table 'sys_file_collection'
 #
 CREATE TABLE sys_file_collection (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -155,8 +155,8 @@ CREATE TABLE sys_file_collection (
 # Table structure for table 'sys_file_reference'
 #
 CREATE TABLE sys_file_reference (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -164,8 +164,8 @@ CREATE TABLE sys_file_reference (
 # Table structure for table 'sys_category'
 #
 CREATE TABLE sys_category (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
 
 
@@ -173,6 +173,6 @@ CREATE TABLE sys_category (
 # Table structure for table 'sys_category_record_mm'
 #
 CREATE TABLE sys_category_record_mm (
-	tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	KEY dummy (tx_oelib_is_dummy_record)
+    tx_oelib_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL,
+    KEY dummy (tx_oelib_is_dummy_record)
 );
