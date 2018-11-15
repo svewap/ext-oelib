f33aa30cb0282e81e736435bfb4db410a5b2f5a2
Oliver Klee <github@oliverklee.de>
2018-11-15 14:01:02 +0100

    [TASK] Move the MapperRegistryTest to the new testing framework (#165)

--- Tests/Unit/Mapper/MapperRegistryTest.php
+++ Tests/Unit/Mapper/MapperRegistryTest.php
@@ -3,6 +3,7 @@
 namespace OliverKlee\Oelib\Tests\Unit\Mapper;
 
 use Nimut\TestingFramework\TestCase\UnitTestCase;
+use OliverKlee\Oelib\Tests\Unit\Mapper\Fixtures\TestingChildMapper;
 use OliverKlee\Oelib\Tests\Unit\Mapper\Fixtures\TestingMapper;
 
 /**
@@ -18,13 +19,177 @@ class MapperRegistryTest extends UnitTestCase
         parent::tearDown();
     }
 
+    ////////////////////////////////////////////
+    // Tests concerning the Singleton property
+    ////////////////////////////////////////////
+
     /**
      * @test
      */
-    public function canReturnNamespacedMapper()
+    public function getInstanceReturnsMapperRegistryInstance()
     {
-        $result = \Tx_Oelib_MapperRegistry::get(TestingMapper::class);
+        self::assertInstanceOf(
+            \Tx_Oelib_MapperRegistry::class,
+            \Tx_Oelib_MapperRegistry::getInstance()
+        );
+    }
 
-        static::assertInstanceOf(TestingMapper::class, $result);
+    /**
+     * @test
+     */
+    public function getInstanceTwoTimesReturnsSameInstance()
+    {
+        self::assertSame(
+            \Tx_Oelib_MapperRegistry::getInstance(),
+            \Tx_Oelib_MapperRegistry::getInstance()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getInstanceAfterPurgeInstanceReturnsNewInstance()
+    {
+        $firstInstance = \Tx_Oelib_MapperRegistry::getInstance();
+        \Tx_Oelib_MapperRegistry::purgeInstance();
+
+        self::assertNotSame(
+            $firstInstance,
+            \Tx_Oelib_MapperRegistry::getInstance()
+        );
+    }
+
+    ////////////////////////////////////////
+    // Test concerning get and setMappings
+    ////////////////////////////////////////
+
+    /**
+     * @test
+     */
+    public function getForEmptyKeyThrowsException()
+    {
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            '$className must not be empty.'
+        );
+
+        \Tx_Oelib_MapperRegistry::get('');
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function getForInexistentClassThrowsException()
+    {
+        \Tx_Oelib_MapperRegistry::get('Tx_Oelib_InexistentMapper');
+    }
+
+    /**
+     * @test
+     */
+    public function getForExistingClassReturnsObjectOfRequestedClass()
+    {
+        self::assertInstanceOf(TestingMapper::class, \Tx_Oelib_MapperRegistry::get(TestingMapper::class));
+    }
+
+    /**
+     * @test
+     */
+    public function getForExistingClassCalledTwoTimesReturnsTheSameInstance()
+    {
+        self::assertSame(
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class),
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class)
+        );
+    }
+
+    ////////////////////////////////////////////
+    // Tests concerning denied database access
+    ////////////////////////////////////////////
+
+    /**
+     * @test
+     */
+    public function getAfterDenyDatabaseAccessReturnsNewMapperInstanceWithDatabaseAccessDisabled()
+    {
+        \Tx_Oelib_MapperRegistry::denyDatabaseAccess();
+
+        self::assertFalse(
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class)->hasDatabaseAccess()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getAfterDenyDatabaseAccessReturnsExistingMapperInstanceWithDatabaseAccessDisabled()
+    {
+        \Tx_Oelib_MapperRegistry::get(TestingMapper::class);
+        \Tx_Oelib_MapperRegistry::denyDatabaseAccess();
+
+        self::assertFalse(
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class)->hasDatabaseAccess()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getAfterInstanceWithDeniedDatabaseAccessWasPurgedReturnsMapperWithDatabaseAccessGranted()
+    {
+        \Tx_Oelib_MapperRegistry::getInstance();
+        \Tx_Oelib_MapperRegistry::denyDatabaseAccess();
+        \Tx_Oelib_MapperRegistry::purgeInstance();
+
+        self::assertTrue(
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class)->hasDatabaseAccess()
+        );
+    }
+
+    /////////////////////////
+    // Tests concerning set
+    /////////////////////////
+
+    /**
+     * @test
+     */
+    public function getReturnsMapperSetViaSet()
+    {
+        $mapper = new TestingMapper();
+        \Tx_Oelib_MapperRegistry::set(
+            TestingMapper::class,
+            $mapper
+        );
+
+        self::assertSame(
+            $mapper,
+            \Tx_Oelib_MapperRegistry::get(TestingMapper::class)
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function setThrowsExceptionForMismatchingWrapperClass()
+    {
+        $mapper = new TestingMapper();
+        \Tx_Oelib_MapperRegistry::set(TestingChildMapper::class, $mapper);
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \BadMethodCallException
+     */
+    public function setThrowsExceptionIfTheMapperTypeAlreadyIsRegistered()
+    {
+        \Tx_Oelib_MapperRegistry::get(TestingMapper::class);
+
+        $mapper = new TestingMapper();
+        \Tx_Oelib_MapperRegistry::set(TestingMapper::class, $mapper);
     }
 }
