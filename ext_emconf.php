a90ba612c1356aa67adcabb262dd75b1560d8791
Oliver Klee <github@oliverklee.de>
2021-09-15 19:18:39 +0200

    [TASK] Drop support for TYPO3 8LTS (#691)
    Fixes #406

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,8 +8,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-8.0.99',
-            'typo3' => '8.7.0-9.5.99',
-            'extbase' => '8.7.0-9.5.99',
+            'typo3' => '9.5.0-9.5.99',
+            'extbase' => '9.5.0-9.5.99',
         ],
         'conflicts' => [
         ],
